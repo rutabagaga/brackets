ce7bdf39110b480462f1f346d852c24028780a0a
rutabagaga <komleva.darya@gmail.com>
2021-03-15 21:45:47 +0300

    feat: modified js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	str = str.split('');
+	for (let a = 0; a < bracketsConfig.length; a++) {
+		 let n = bracketsConfig[a][0];
+		 let x = bracketsConfig[a][1];
+			  for (let i=0; i < str.length; i++) {
+					if (str[i] == n && str[i+1] == x) {
+						 str.splice(i, 2);
+						 i = -1;
+						 a = -1;
+					}
+			  }
+	}
+	return !(str.length);
 }
